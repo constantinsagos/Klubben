3edb287a871da1875d73bf5b0f574958d4015651
constantinsagos <155970332+constantinsagos@users.noreply.github.com>
2025-08-31 22:26:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,26 @@ document.getElementById("reviewForm").addEventListener("submit", function(e) {
   e.preventDefault();
 
   const movie = document.getElementById("movieName").value.trim();
-  const genre = document.getElementById("genre").value.trim();
   const cat1 = parseInt(document.getElementById("cat1").value);
   const cat2 = parseInt(document.getElementById("cat2").value);
   const cat3 = parseInt(document.getElementById("cat3").value);
   const cat4 = parseInt(document.getElementById("cat4").value);
   const cat5 = parseInt(document.getElementById("cat5").value);
+  const cat6 = parseInt(document.getElementById("cat6").value);
 
-  const average = ((cat1 + cat2 + cat3 + cat4 + cat5) / 5).toFixed(1);
+  const recommend = document.querySelector('input[name="recommend"]:checked').value;
+
+  const average = ((cat1 + cat2 + cat3 + cat4 + cat5 + cat6) / 6).toFixed(1);
 
   const review = {
     movie,
-    genre,
     cat1,
     cat2,
     cat3,
     cat4,
     cat5,
+    cat6,
+    recommend,
     average
   };
 
